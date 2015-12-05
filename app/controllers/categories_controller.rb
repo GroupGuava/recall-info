9484e10a87ab682aa7c8d3149d839ffd74e25212
rameshv132 <rameshv@oregonstate.edu>
2015-12-05 14:37:48 -0800

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,52 @@
 class CategoriesController < ApplicationController
   def index
+  @recallItems = Item.all
+  
+	
+  
+ # puts "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHh"
+  
   end
+  
+  
+	
+
+  def create
+	
+		
+  end
+
+ 
+  def parse_comments
+    
+    #do your stuff with comments_from_form here
+  end
+
+
+
+
+
+
+  
+
+  
+  
+  
+  def list
+	#@recallItems = Item.all
+  end
+  
+  def show
+	@recallItems = Item.first
+  end
+  
+  def iterate
+	
+	@dat = "NENEEEEEENENENENENNENEEEEEEEEEEENEE";
+	param1 = params[:param1] # "value1"
+	param2 = params[:param2] # "value2"
+	puts "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHh"
+	
+  end
+  
 end
